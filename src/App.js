eede025d0e30a10342e1db71c1966599c9b5067d
Aditya Chatap <adityachatap123@gmail.com>
2023-03-25 21:33:27 +0530

    Started the new project .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { } from '@material-ui/core';
+import React, { } from 'react';
+import { Route } from 'react-router-dom';
 
+import './App.css';
+import Header from './components/Header';
+// import Header from './components/Header'
+import CoinPage from './Pages/CoinPage';
+import Homepage from './Pages/Homepage';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Header/>
+      <Route path='/' component={Homepage} exact />
+      <Route path='/coins/:id' component={CoinPage}/>
+
     </div>
   );
 }
-
 export default App;
